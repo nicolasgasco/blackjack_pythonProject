49a8ec9988382d389a30636022ec38f58b246657
Nicolas Gasco <nicolas.gasco92@gmail.com>
2020-11-20 08:16:13 +0100

    Started creating main scheme

--- main.py
+++ main.py
@@ -1,9 +1,39 @@
+import random
 import time
 from actions import *
+from deck import *
+from splash_screen import *
 
-user_name = ask_name()
+
+deck_list = generate_deck_list()
+played_cards = [] 
+player_hand = []
+dealer_hand = []
+stand = 0
+hit = 0
+
+# Used to print the game's logo
+splash_screen()
+
+# Let's welcome the player
+player_name = ask_name()
+
+welcome(player_name)
+
+# Let's deal cards
+first_hand(player_name, player_hand)
+isBlackJack
+
+# Stand or hit after receiving cards?
+round2 = stand_or_hit()
+if not round2:
+    if_stand()
+elif round2:
+    if_hit()
+    # it checks if bust
+        if bust:
+            game over
+        elif not bust:
+            another card
 
 
-welcome_player(user_name)
-
-first_hand()
