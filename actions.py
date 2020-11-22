8e7ca12c1947118cc4157a9c01279bcd52f28674
Nicolas Gasco <nicolas.gasco92@gmail.com>
2020-11-22 22:52:42 +0100

    Completed all logic of the game

--- actions.py
+++ actions.py
@@ -1,55 +1,49 @@
 import random
 import time
 from deck import *
-from splash_screen import *
 
-
-# Full deck and cards that are played
-deck_list = generate_deck_list()
-deck_dict = generate_deck_dict()
-played_cards = [] 
-player_hand = []
-dealer_hand = []
-
-
-def ask_name():
-   user_name = input("Hi! What's your name? Write it here: ")
-   print("\n")
+def welcome():
+   user_name = input("Hi! Before we start, write your name here: ")
+   while user_name == "" or user_name == " ":
+      user_name = input("Please insert a valid name: ")
+   user_name = user_name.strip().title()
+   print(f"\nHi, {user_name}! Welcome to Python Blackjack. This is a simple game,"
+         " but it's a lot of fun. You don't need to place a bet to play.\n")
    return user_name
+      
 
-def welcome(user_name):
-   print(f"Hi, {user_name.title()}! Welcome to Python Blackjack. This is a simple game,"
-    	" but it's a lot of fun. You don't need to place a bet to play.\n")
-   print("Let's start...\n")
+def first_hand(user_name, player_hand, dealer_hand, deck_list):
+   """Function to deal the first hand of a Blackjack round.
+   Player gets dealt two cards face up, dealer only one face up."""
 
-def first_hand(user_name, player_hand, dealer_hand):
-	"""Function to deal the first hand of a Blackjack round.
-	Player gets dealt two cards face up, dealer only one face up."""
+   for i in range(2):
+   # One random card to the player
+      card_user = random.choice(deck_list)
+      deck_list.remove(card_user)
+      player_hand.append(card_user)
+##      # Adding to played cards just in case it's useful in the future
+##      played_cards.append(card_user)
 
-	for i in range(2):
-		# One random card to the player
-		card_user = random.choice(deck_list)
-		deck_list.remove(card_user)
-		player_hand.append(card_user)
-		# Adding to played cards just in case it's useful in the future
-		played_cards.append(card_user)
-		
 
-		# One random card to the dealer
-		card_dealer = random.choice(deck_list)
-		deck_list.remove(card_dealer)
-		dealer_hand.append(card_dealer)
-		# Adding to played cards just in case it's useful in the future
-		played_cards.append(card_user)
+      # One random card to the dealer
+      card_dealer = random.choice(deck_list)
+      deck_list.remove(card_dealer)
+      dealer_hand.append(card_dealer)
+##      # Adding to played cards just in case it's useful in the future
+##      played_cards.append(card_dealer)
 
-	print(f"{user_name.title()}, you have been dealt..."
-		f" {cardname_fromTuple(player_hand, player_hand[0])} and "
-		f"{cardname_fromTuple(player_hand, player_hand[1])}.\n")
+   print("Let's start a new game of Python Blackjack.\n")
 
-	# time.sleep(2)
+   print(f"{user_name.title()}, you have been dealt..."
+   f" {cardname_fromTuple(player_hand, player_hand[0])} and "
+   f"{cardname_fromTuple(player_hand, player_hand[1])}.\n")
 
-	print(f"The dealer has one covered card and a "
-		f"{cardname_fromTuple(dealer_hand, dealer_hand[0])}.\n")
+   # time.sleep(2)
+
+   print(f"The dealer has one covered card and a "
+   f"{cardname_fromTuple(dealer_hand, dealer_hand[0])}.\n")
+
+
 def isNatural(card_hand):
     """Function to check if two first cards are a natural (10 plus ace)"""
     if (card_hand[0])[2] == 1 and (card_hand[1])[2] == 10:
@@ -74,16 +68,21 @@ def stand_or_hit():
       print("You chose to hit.\n")
       return True
 
-def if_stand(card_hand):
+def if_stand(dealer_hand):
    """Function to play second hand, when player decided to stand"""
    sum_hand = 0
-   for card in card_hand:
+   for card in dealer_hand:
       # 11 has precedence over 1 when the dealer plays
       if card[2] == 1:
          sum_hand += 11
       else:
          sum_hand += card[2]
-   card = [cardname_fromTuple(card_hand, card) for card in card_hand]
+
+   # We have to clear the cases where 11 generates bust
+   if sum_hand > 21:
+      sum_hand -= 10
+
+   card = [cardname_fromTuple(dealer_hand, card) for card in dealer_hand]
    print(f"Dealer sum is {sum_hand}. He has {card}") 
    if sum_hand >= 17:
       #In this case stand
@@ -92,21 +91,80 @@ def if_stand(card_hand):
       #In this case hit
       return True
 
-def if_hit(player_hand):
+def stand_whoWon (dealer_hand, player_hand):
+   """Function to determine who won when both the dealer and the player are standing (two cards)"""
+   sum_dealer = 0
+   for card in dealer_hand:
+      # 11 has precedence over 1 when the dealer plays
+      if card[2] == 1:
+         sum_dealer += 11
+      else:
+         sum_dealer += card[2]
+   if sum_dealer > 21:
+      sum_dealer -= 10
+   else:
+      pass
+
+   sum_player = 0
+   for card in player_hand:
+      if card[2] == 1:
+         # In this case ace must be 11 because player has only two cards
+         sum_player += 11
+      else:
+         sum_player += card[2]
+         
+  #CHANGE SUM TO INCLUDE 1       
+   if sum_player > sum_dealer:
+      print("Your cards are higher than the dealer's. Congratulations, you won!")
+   elif sum_player == sum_dealer:
+      print("You and the dealer have the same total value. It's a tie!")
+   elif sum_player < sum_dealer:
+      print("Your cards are lower than the dealer's. You lost!")
+
+def dealer_card(dealer_hand, deck_list):
+   """Function to give one random card to the dealer"""
+   # One random card to the dealer
+   card_dealer = random.choice(deck_list)
+   deck_list.remove(card_dealer)
+   dealer_hand.append(card_dealer)
+##   # Adding to played cards just in case it's useful in the future
+##   played_cards.append(card_dealer)
+
+def isBust_dealer(dealer_hand):
+   """"Function to determine if dealer is bust with current hand"""
+   sum_dealer = 0
+   sum1 = 0
+   sum11 = 0
+   for card in dealer_hand:
+      if card[2] == 1:
+         sum1 += 1
+         sum11 += 11
+      else:
+         sum_dealer += card[2]
+   
+   total1 = sum_dealer + sum1
+   total11 = sum_dealer + sum11
+   if total11 > 21 and total1 > 21:
+      print("Dealer is bust")
+      return True
+   elif total11 > 21 and total1 < 21:
+      print("Dealer is not bust")
+      return False
+   else:
+      print("Dealer is not bust")
+      
+         
+   
+
+def if_hit(player_hand, deck_list):
    """Function to play second hand, when player can either receive another card or stand."""
    card_user = random.choice(deck_list)
    deck_list.remove(card_user)
    player_hand.append(card_user)
-   # Just in case I need it in the future
-   played_cards.append(card_user)
+##   # Just in case I need it in the future
+##   played_cards.append(card_user)
    print(f"You have received a {cardname_fromTuple(player_hand, player_hand[-1])}.\n")
 
-##        card_dealer = random.choice(deck_list)
-##        deck_list.remove(card_dealer)
-##        dealer_hand.append(card_dealer)
-##        # Just in case I need it in the future
-##        played_cards.append(card_dealer)
-##        print(f"The dealer has received a {cardname_fromTuple(dealer_hand, dealer_hand[2])}.\n")
 
 def isBust(hand_cards):
       # First I check if there are some 1s
@@ -169,21 +227,6 @@ def isBlackJack(hand_cards):
          print(f"Ouch! The dealer won!")
    else:
       return False
-##
-##def who_won(player, val1=1, dealer, val2=11):
-##   sum_hand1 = 0
-##   for card in player:
-##      if card[2] == 1:
-##         sum_hand1 += val1
-##      else:
-##         sum_hand1 += card[2]
-##
-##   sum_hand1 = 0
-##   for card in player:
-##      if card[2] == 1:
-##         sum_hand1 += val2
-##      else:
-##         sum_hand1 += card[2]
 
       
       
--- main.py
+++ main.py
@@ -5,70 +5,97 @@ from deck import *
 from splash_screen import *
 
 
-deck_list = generate_deck_list()
-played_cards = [] 
-player_hand = []
-dealer_hand = []
-stand = 0
-hit = 0
 
 # Used to print the game's logo
 splash_screen()
 
 # Let's welcome the player
-player_name = ask_name()
-
-welcome(player_name)
+player_name = welcome()
 
 # Biggest loop for the actual gaming part
-# while True:
-# Let's deal cards
-first_hand(player_name, player_hand, dealer_hand)
+big_loop = True
+while True:
+    # If not declared here, they get summed game after game
+    deck_list = generate_deck_list()
+    played_cards = [] 
+    player_hand = []
+    dealer_hand = []
+    stand = 0
+    hit = 0
+    # Let's deal cards
+    first_hand(player_name, player_hand, dealer_hand, deck_list)
 
-if isNatural(player_hand) and isNatural(dealer_hand):
-    print("You both have 21! It's a TIE!")
-elif isNatural(player_hand) and not isNatural(dealer_hand):
-    print("You have 21. You won!")
-elif not isNatural(player_hand) and isNatural(dealer_hand):
-    print("The dealer has 21. You lost!")
-else:
-    pass
+    if isNatural(player_hand) and isNatural(dealer_hand):
+        print("The dealer can show his cards now...")
+        print("You both have 21! It's a TIE!")
+        big_loop = False
+    elif isNatural(player_hand) and not isNatural(dealer_hand):
+        print("You have 21. You won!")
+        big_loop = False
+    elif not isNatural(player_hand) and isNatural(dealer_hand):
+        print("The dealer can show his cards now...")
+        print("The dealer has 21. You lost!")
+        big_loop = False
+    else:
+        pass
 
-# This loop is for continuing asking for a new card
-loop = True
-while loop == True:
-    # Let's ask if they're standing or hitting
-    round2 = stand_or_hit()
-    
-    #Player chooses to stand
-    if not round2:
-        print(f"The dealer cover card is {cardname_fromTuple(dealer_hand, dealer_hand[1])}") 
-        # Let's check if dealer is standing or hitting
-        dealer_action = if_stand(dealer_hand)
-        # Dealer is hitting
-        if dealer_action:
-            print("Dealer is hitting")
-            break
-        # Dealer is standing
-        if not dealer_action:
-            print(f"Dealer is standing. He's sum is {dealer_action}")
-            break
-    
-    #Player chooses to hit
-    if round2:
-        if_hit(player_hand)
-        isBlackJack(player_hand)
-
-        # Check if player is bust
-        bust = isBust(player_hand)
+    # This loop is for continuing asking for a new card
+    loop = True
+    while loop == True:
+        # Let's ask if they're standing or hitting
+        round2 = stand_or_hit()
         
-        #if bust
-        if bust:
-            print("Game over.")
-            print("Prepare. A new game will start...\n\n\n")
-            break
+        #Player chooses to stand
+        if not round2:
+            print(f"The dealer covered card is {cardname_fromTuple(dealer_hand, dealer_hand[1])}") 
+            # Let's check if dealer is standing or hitting
+            dealer_loop = True
+            while dealer_loop == True:
+                dealer_action = if_stand(dealer_hand)
+
+                # Dealer is hitting
+                if dealer_action:
+                    print("Dealer is hitting")
+                    # Dealer receives one more card
+                    dealer_card(dealer_hand, deck_list)
+                    # Let's check if he's bust
+                    dealer_bust = isBust_dealer(dealer_hand)
+
+                    # dealer is bust
+                    if dealer_bust:
+                        print("The dealer is bust! You won!")
+                        print("\n\n\n\n\n\n\n\n\n\n")
+                        dealer_loop = False
+                        loop = False
+
+                    #dealer is not bust
+                    elif not dealer_bust:
+                        continue
+                        # hit again
+            
+            # Dealer is standing
+                elif not dealer_action:
+                    print(f"Dealer is standing.")
+                    stand_whoWon(dealer_hand, player_hand)
+                    print("\n\n\n\n\n\n\n\n\n\n")
+                    dealer_loop = False
+                    loop = False
         
-        #if not bust
-        elif not bust:
-            print("The game goes on...")
-            # Loops starts all over again
+        #Player chooses to hit
+        if round2:
+            if_hit(player_hand, deck_list)
+            isBlackJack(player_hand)
+
+            # Check if player is bust
+            bust = isBust(player_hand)
+            
+            #if bust
+            if bust:
+                print("Game over.")
+                print("Prepare. A new game will start...\n\n\n")
+                break
+            
+            #if not bust
+            elif not bust:
+                print("The game goes on...")
+                # Loops starts all over again
